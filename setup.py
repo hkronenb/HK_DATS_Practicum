4f57cdccef855fb8b5f3fe372e7c4d539df44a6e
Hannah Kronenberg <hannah.kronenberg@gmail.com>
2020-10-29 18:23:45 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An analysis of the impact of COVID in Philadelphia using Safegraph data',
+    author='Hannah Kronenberg',
+    license='MIT',
+)
